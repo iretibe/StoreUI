44d84df1d7e5aded784fae8c1488a88e0d96cadc
iretibe <yessouf2009@yahoo.fr>
2023-03-15 13:13:37 +0000

    Latest code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,36 +9,41 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', 
-    component: HomeComponent, 
+    path: '',
+    component: HomeComponent,
     data: {breadcrumb: 'Home'}
   },
   {
-    path: 'test-error', 
-    component: TestErrorComponent, 
+    path: 'test-error',
+    component: TestErrorComponent,
     data: {breadcrumb: 'Test Errors'}
   },
   {
-    path: 'server-error', 
-    component: ServerErrorComponent, 
+    path: 'server-error',
+    component: ServerErrorComponent,
     data: {breadcrumb: 'Server Error'}
   },
   {
-    path: 'not-found', 
-    component: NotFoundComponent, 
+    path: 'not-found',
+    component: NotFoundComponent,
     data: {breadcrumb: 'Not Found'}
   },
   // {path: 'shop', component: ShopComponent},
   // {path: 'shop/:id', component: ProductDetailsComponent},
   {
-    path: 'shop', 
-    loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), 
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
     data: {breadcrumb: 'Shop'}
+  },
+  {
+    path: 'basket',
+    loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+    data: {breadcrumb: 'Basket'}
   },
   // {path: '**', redirectTo: '', pathMatch: 'full'}
   {
-    path: '**', 
-    redirectTo: 'home', 
+    path: '**',
+    redirectTo: 'home',
     pathMatch: 'full'
   }
 ];
---
+++ src/app/basket/basket-routing.module.ts
@@ -0,0 +1,19 @@
+import { BasketComponent } from './basket.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BasketComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class BasketRoutingModule { }
